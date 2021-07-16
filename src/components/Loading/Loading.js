bbf868246cfce16da68a8e3ef59470bd07f12efd
stormymayday <stormymayday7@gmail.com>
2021-07-15 20:11:35 -0600

    Added Bext360 logo to a Loading component.

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import logo from '../../images/BextLogo.png';
+
 const Loading = () => {
     return (
         <>
-            <h1 style={{ textAlign: `center`, marginTop: `20rem` }}>Data Powered by BEXT360</h1>
+            <h1 style={{ textAlign: `center`, marginTop: `10rem` }}>Data Powered by</h1>
+
+            <div style={{
+                'margin-top': '2em',
+                'display': 'flex',
+                'align-items': 'center',
+                'justify-content': 'center'
+            }}>
+
+                <img src={logo} style={{ 'width': '15em' }} ></img>
+            </div>
             <div className="loader">
             </div>
 
