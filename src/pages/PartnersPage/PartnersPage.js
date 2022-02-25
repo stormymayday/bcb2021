76e7bc2eae3d81a96ef693c820717f6db6fb6221
stormymayday <stormymayday7@gmail.com>
2022-02-24 19:36:56 -0700

    Added GA to the Partners page.

--- src/pages/PartnersPage/PartnersPage.js
+++ src/pages/PartnersPage/PartnersPage.js
@@ -42,6 +42,15 @@ import './PartnersPage.css';
 
 export const PartnersPage = () => {
 
+    useEffect(() => {
+
+        // Google Analytics initiazation
+        // ReactGA.initialize(`${process.env.REACT_APP_GA_ID}`);
+        // Google Analytics Page View report
+        ReactGA.pageview(window.location.pathname + window.location.search);
+
+    }, [])
+
 
     return (
 
